493ad7d56052988e1a96ed84d9393fd2e68def39
Mightyolu <idowuolumide30@gmail.com>
2022-12-14 10:39:58 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,10 @@
-#!/bin/bash
-/**
- *
- *  * main-entry point for the program.
- *
- *   *
- *
- *    * Return:0 if no errors, return non zero if errors
- *
- *     */
-
+#include <stdio.h>
+/** 
+ * main - entring point into the program 
+ * Return: 0 for success
+ */ 
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0)
 }
