6e1ce1900200bf18283bdf48602a496569ffcca8
Mightyolu <idowuolumide30@gmail.com>
2022-12-21 09:23:48 -0800

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * rev_string - reverses a string.
  * @s: input string.
@@ -17,8 +17,10 @@ void rev_string(char *s)
 	}
 	str = s;
 
-	for (j = i + 1; j > 0; j--)
-	{
+	for (i = 0; i < (count - 1); 1++
+			{
+			for (j = i + 1; j > 0; j--)
+			{
 		temp = *(str + (j -1);
 				*(str + j) = *(str + (j - 1));
 				*(str + (j - 1)) = temp;
