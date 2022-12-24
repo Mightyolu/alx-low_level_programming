1e5abad2b25924df86fb06088c7193f10d88b96a
Mightyolu <idowuolumide30@gmail.com>
2022-12-23 22:49:02 -0800

    putc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-main.hi
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include "main.h"
+
+/**
+ *  _isupper - function that checks uppercase alphas
+ *  @c: an integer
+ *  Return: something but success
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
