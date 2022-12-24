aaa1bc4ca2b3bcca29f29d48772d58eb386d6dbd
Mightyolu <idowuolumide30@gmail.com>
2022-12-23 23:36:11 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - entering point into the program
- * Return: 0 for success
+ * main- Program entry point
+ *
+ * Return: 1 on success.
  */
 int main (void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+		return (1);
 }
