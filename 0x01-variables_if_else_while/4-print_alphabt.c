b8b917839c39ecedec76353e9234268b42ad1319
Mightyolu <idowuolumide30@gmail.com>
2022-12-24 03:17:57 -0800

    4print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,51 +1,28 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
-/**
+/*
+ * main - returns alphabets both except e ad q
  *
- *  * main - returns alphabets both except e ad q
+ * Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     *
- *
- *      */
-
-
+ */
 
 int main(void)
-
 {
-
 		char ch = 'a';
-
-
-
+		
 			while (ch <= 'z')
-
-					{
-
-								if ((ch != 'e') && (ch != 'q'))
-
-											{
-
-															putchar(ch);
-
-																	}
-
-										ch++;
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+			{
+				if ((ch != 'e') && (ch != 'q'))
+				{
+					putchar (ch);
+				}
+				ch++;
+			}
+			putchar ('\n');
+}
+putchar ('\n');
+return (0);
 }
