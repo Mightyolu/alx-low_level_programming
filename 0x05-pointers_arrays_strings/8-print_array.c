152aa18b04e83cca744ac5a649e997e9714e36cb
Mightyolu <idowuolumide30@gmail.com>
2022-12-25 15:18:27 -0800

    prints8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,8 +15,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < (n - 1));
-		printf (",");
+		if (i < (n - 1))
+			printf(", ");
 	}
 	printf("\n");
 }
