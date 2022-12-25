72d3ffb51d103516b356619db0f334ad4d46441a
Mightyolu <idowuolumide30@gmail.com>
2022-12-25 14:56:21 -0800

    put3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line.
+ *  _puts - prints a string, followed by a new line.
  * @str: input string.
  * Return: no return.
  */
@@ -17,7 +17,7 @@ void _puts(char *str)
 			_putchar('\n');
 			break;
 		}
-		-putchar(str[count]);
+		_putchar(str[count]);
 		count++;
 	}
 }
