0a05a2fbd41597c1808cb7121aa2bc9e4074c8fe
Mightyolu <idowuolumide30@gmail.com>
2022-12-25 14:03:36 -0800

    print9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+ #include <stdio.h>
+/**
+ * main - progdram entry point
+ *
+ *  Return: 0 success, non-zero error
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
