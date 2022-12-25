38a2e3808998304276c87e80f61131210e728770
Mightyolu <idowuolumide30@gmail.com>
2022-12-25 15:45:59 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,87 +1,37 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- *
- *  * _atoi - converts a string to an integer.
- *
- *   * @s: input string.
- *
- *    * Return: integer.
- *
- *     */
+ * _atoi - function convers string to an integer
+ * @s: pointer parameter
+ * Return: returns an integer value
+ */
 
 int _atoi(char *s)
-
 {
+	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
 
-		int i, d, n, len, f, digit;
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 
+		if (*(s + count) == '-')
+			k *= -1;
 
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
 
-			i = 0;
-
-				d = 0;
-
-					n = 0;
-
-						len = 0;
-
-							f = 0;
-
-								digit = 0;
-
-
-
-									while (s[len] != '\0')
-
-												len++;
-
-
-
-										while (i < len && f == 0)
-
-												{
-
-															if (s[i] == '-')
-
-																			++d;
-
-
-
-																	if (s[i] >= '0' && s[i] <= '9')
-
-																				{
-
-																								digit = s[i] - '0';
-
-																											if (d % 2)
-
-																																digit = -digit;
-
-																														n = n * 10 + digit;
-
-																																	f = 1;
-
-																																				if (s[i + 1] < '0' || s[i + 1] > '9')
-
-																																									break;
-
-																																							f = 0;
-
-																																									}
-
-																			i++;
-
-																				}
-
-
-
-											if (f == 0)
-
-														return (0);
-
-
-
-												return (n);
-
+	for (i = count - size; i < count; i++)
+	{
+		j = j + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (j * k);
 }
