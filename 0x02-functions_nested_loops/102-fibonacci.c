3416bbb9653c3fde7365fdf7d4ff00600a63d96b
Mightyolu <idowuolumide30@gmail.com>
2022-12-28 03:51:53 -0800

    102times

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+ #include <stdio.h>
+
+/**
+ * main - : Program that prints the first 50 Fibonacci numbers
+ * Return: Always 0 if no error
+ */
+
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+
+		else
+		{
+			printf("%ld\n", j);
+		}
+
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	return (0);
+}
