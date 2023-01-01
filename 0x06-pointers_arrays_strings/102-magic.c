31855357999cd7a43454439a6766932d1823cbf4
Mightyolu <idowuolumide30@gmail.com>
2022-12-31 16:12:57 -0800

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - This is the code pattern for array
+ *
+ *   * Remember: -you are not allowed to use a, and you are not allowed to modify p
+ *
+ *    * -only one statement and
+ *
+ *     * -you are not allowed to code anything else than this line of code
+ *
+ *      * main: Don't know why betty keeps asking another main description
+ *
+ *       */
+
+
+
+int main(void)
+
+{
+
+		int n;
+
+			int a[5];
+
+				int *p;
+
+
+
+					a[2] = 1024;
+
+						p = &n;
+
+							p[5] = 98;
+
+								/*...so that this prints 98\n */
+
+								printf("a[2] = %d\n", a[2]);
+
+
+
+									return (0);
+
+}
