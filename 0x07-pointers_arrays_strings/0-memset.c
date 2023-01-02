6038db319e7f1cc927ea3fe70aba9da428e50dd3
Mightyolu <idowuolumide30@gmail.com>
2023-01-02 11:29:22 -0800

    semset git commit -m semset rm git commit -m \semset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,10 +3,11 @@
 /**
  * _memset - fills the memory with a constant bite
  * @n: The specified number of bytes
- *  @s: The pointer of type char
- *  @b: The constant byte
+ * @s: The pointer of type char
+ * @b: The constant byte
+ * Return: Returns a pointer to the memory area 's'
  */
-Return: Returns a pointer to the memory area 's'
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
