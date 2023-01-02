3e3d7348ea8f8cc3bce13cd76a2b01fa53286ff9
Mightyolu <idowuolumide30@gmail.com>
2023-01-02 11:24:36 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+
+/**
+ * _memset - fills the memory with a constant bite
+ * @n: The specified number of bytes
+ *  @s: The pointer of type char
+ *  @b: The constant byte
+ */
+Return: Returns a pointer to the memory area 's'
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
