bc3ca1f7d447436432355cfec5e89fd3cb848a60
Mightyolu <idowuolumide30@gmail.com>
2023-01-04 09:14:57 -0800

    putts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  _puts_recursion - Prints a string, followed by a new line.
- *  @s: The string to be printed.
+ * _puts_recursion - Print a string, followedby a new line.
+ * @s: The string to be printed.
  */
 
 void _puts_recursion(char *s)
