c011c9f47f39ee28ba10941a0b6d43f419d0bc20
Mightyolu <idowuolumide30@gmail.com>
2023-01-13 18:18:13 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,7 +6,8 @@
  * _atoi_digit - convert a char to integer.
  * @x: character to convert.
  * Return: integer.
- */
+ **/
+
 int _atoi_digit(char x)
 {
 	unsigned int res;
@@ -20,7 +21,7 @@ int _atoi_digit(char x)
  * _isNumber - Define if a string is a number.
  * @argv: Pointer to string.
  * Return: success (0).
- */
+ **/
 int _isNumber(char *argv)
 {
 	int i;
@@ -32,11 +33,12 @@ int _isNumber(char *argv)
 }
 
 /**
- * *_calloc - allocate array of size * nmemb.
+ * _calloc - allocate array of size * nmemb.
  * @nmemb: number of elements.
  * @size: size of element.
  * Return: pointer to array.
- */
+ **/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *tab;
@@ -61,7 +63,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
  * @a3: array for result.
  * @lena: length of array a3.
  * Return: pointer to array.
- */
+ **/
+
 void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 {
 	int mul = 0, i, k;
@@ -89,7 +92,7 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
  * print_array - print all digits of array.
  * @nb: number of elements to print.
  * @a: array of elements.
- */
+ **/
 void print_array(char *a, int nb)
 {
 	int i = 0;
