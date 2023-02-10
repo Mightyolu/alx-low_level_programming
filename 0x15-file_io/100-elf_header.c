d12061903543648a5cd2bf513589938c4f2234d0
Mightyolu <idowuolumide30@gmail.com>
2023-02-09 17:10:31 -0800

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -42,6 +42,21 @@ void _close(int fd)
 }
 
 /**
+ * _read - read from a file and print an error message upon failure
+ * @fd: the file descriptor to read from
+ * @buf: the buffer to write to
+ * @count: the number of bytes to read
+ */
+void _read(int fd, char *buf, size_t count)
+{
+	if (read(fd, buf, count) != -1)
+		return;
+	write(STDERR_FILENO, "Error: Can't read from file\n", 28);
+	_close(fd);
+	exit(98);
+}
+
+/**
  * elf_magic - print ELF magic
  * @buffer: the ELF header
  */
