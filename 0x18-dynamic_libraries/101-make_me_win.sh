ca0975a8a1338b3e26453bd136dac94e16a1129b
Mightyolu <idowuolumide30@gmail.com>
2023-03-24 17:00:16 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/IZZYLOVE/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD =/tmp/libtest.so
+wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
